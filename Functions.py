2670fd11f7d57d0ab25ac6a2d01beabe483126fc
enrict <enric_trillo@outlook.com>
2021-08-22 17:24:29 +0100

    Functions

---
+++ Functions.py
@@ -0,0 +1,8 @@
+def programming_checker():
+    love = input('Which programming language do you love?: ')
+    hate = input('Which programming language do you hate?: ')
+
+    print(f"Answer: You said you love {love}")
+    print(f"Answer: You said you hate {hate}")
+
+programming_checker()
